a707a55d3bd17c7e137785c3dbddb20c3201bf7b
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 22:12:21 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,9 @@ def third_challenge
         :age => "54"#your key/value pairs here
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"#your key/value pairs here
         },
       :hero_friends => []
    },
